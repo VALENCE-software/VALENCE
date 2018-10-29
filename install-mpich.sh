27238b2e843135be21a4081c76272dc3a6163675
Colleen Bertoni <bertoni@anl.gov>
2018-10-28 22:41:01 -0500

    Update install-mpich.sh

--- install-mpich.sh
+++ install-mpich.sh
@@ -4,7 +4,7 @@ wget --no-check-certificate http://www.mpich.org/static/downloads/3.2/mpich-3.2.
 tar -xf mpich-3.2.tar.gz
 rm mpich-3.2.tar.gz
 cd mpich-3.2
-./configure --enable-fast=all  --enable-shared --prefix=../
+./configure --enable-fast=all  --enable-shared --prefix=/home/travis/
 make -j4
 make install
 cd ..
