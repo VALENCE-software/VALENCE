fb748e25363698f8d05a6995eb0d5ce61a51dc42
Colleen Bertoni <bertoni@anl.gov>
2018-10-28 20:03:06 -0500

    Adding install script for mpich

---
+++ install-mpich.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget --no-check-certificate http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+tar -xf mpich-3.2.tar.gz
+rm mpich-3.2.tar.gz
+cd mpich-3.2
+../configure --enable-fast=all  --enable-shared --prefix=/usr/local
+make -j4
+make install
+cd ..
