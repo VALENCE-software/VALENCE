f37afae017bfe4bd71978c9fc1ee8227f5c73991
Colleen Bertoni <bertoni@anl.gov>
2018-11-13 16:35:52 +0000

    Bug fix to parallel runscript--removing appending

--- nitrogen/parallel_run_script.sh
+++ nitrogen/parallel_run_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mpirun -f $COBALT_NODEFILE -np 56 -ppn 56 /home/bertoni/VALENCE/bin/valence < new_input | tee -a out
+mpirun -f $COBALT_NODEFILE -np 56 -ppn 56 /home/bertoni/VALENCE/bin/valence < new_input | tee out
 echo "new orbitals"
 cat orbitals
 grep "total energy" out | awk '{print $3}' > energy_output
-#rm out
-#rm new_input
+rm out
+rm new_input
